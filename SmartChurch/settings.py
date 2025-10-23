ddc723b8be512b8187f1468d12f7ed38d8a98375
SANGAMARTIN99 <martinsanga230@gmail.com>
2025-10-23 15:45:34 +0300

    static url path2

--- SmartChurch/settings.py
+++ SmartChurch/settings.py
@@ -206,7 +206,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 
 MEDIA_URL = '/media/'
